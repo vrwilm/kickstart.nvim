b5203b29e483693a3502c06fd0c0cd7f9d19583b
victor <vwilm@outlook.com>
2024-05-23 19:28:42 -0700

    add init_py.lua as a reference

--- init.lua
+++ init.lua
@@ -586,10 +586,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        basedpyright = {},
+        -- basedpyright = {},
         -- clangd = {},
         gopls = {},
         -- pyright = {},
+        pylsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
